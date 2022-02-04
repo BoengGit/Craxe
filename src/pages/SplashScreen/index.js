ecc0118f9b4bb3ed0300063a26605d083d9dcfcf
bajang riau <edukasiasqor@gmail.com>
2022-02-04 22:07:54 +0700

    membuat tampilan splash screen

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -11,12 +11,13 @@ export default class SplashScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View>
-          <View>
-            <Logo />
-            <Text style={styles.text}>Dining and Delivery Restaurant App</Text>
-          </View>
+        <HatIcon style={styles.hatIcon} />
+        <View style={styles.div}>
+          <Logo />
+          <Text style={styles.text1}>Dining and Delivery</Text>
+          <Text style={styles.text2}>Restaurant App</Text>
         </View>
+        <BowlIcon style={styles.bowlIcon} />
       </View>
     );
   }
@@ -25,9 +26,37 @@ export default class SplashScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  text: {
+  div: {
+    width: 307,
+    height: 164,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text1: {
+    marginTop: 40,
     fontSize: 28,
     fontFamily: 'Poppins-Regular',
+    textAlign: 'center',
+    color: 'rgba(0,0,0,0.8)',
+  },
+  text2: {
+    marginTop: -12,
+    fontSize: 28,
+    fontFamily: 'Poppins-Regular',
+    textAlign: 'center',
+    color: 'rgba(0,0,0,0.8)',
+  },
+  hatIcon: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  bowlIcon: {
+    position: 'absolute',
+    bottom: 0,
+    right: 10,
   },
 });
